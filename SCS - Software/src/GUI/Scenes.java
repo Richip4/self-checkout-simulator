7692191878e8912421d3ba4a17e569c35662957d
Joshua Plosz <joshua.plosz@gmail.com>
2022-04-02 15:30:43 -0600

    started implementing individual scenes

--- SCS - Software/src/GUI/Scenes.java
+++ SCS - Software/src/GUI/Scenes.java
@@ -1,5 +1,49 @@
 package GUI;
 
-public class Scenes {
+import javax.swing.JFrame;
 
+public class Scenes {
+	
+	public final int SC_OVERVIEW 		= 0;
+	public final int AS_TOUCH 			= 1;
+	public final int SCS_OVERVIEW 		= 2;
+	public final int SCS_TOUCH 			= 3;
+	public final int SCS_KEYBOARD 		= 4;
+	public final int SCS_CARDREADER 	= 5;
+	public final int SCS_MAINTENANCE 	= 6;
+	
+	public JFrame getScene(int scene) {
+		switch (scene) {
+		case SC_OVERVIEW:
+			return null;
+		case AS_TOUCH:
+			return null;
+		case SCS_OVERVIEW:
+			return null;
+		case SCS_TOUCH:
+			return null;
+		case SCS_KEYBOARD:
+			return null;
+		case SCS_CARDREADER:
+			return null;
+		case SCS_MAINTENANCE:
+			return null;
+		}
+		return null;
+	}
+
+	// Self-Checkout Overview Scene
+	
+	// Attendant Station Touch Screen Scene
+	
+	// Self-Checkout Station Overview Scene
+	
+	// Self-Checkout Station Touch Screen Scene
+	
+	// Self-Checkout Station Keyboard Scene
+	
+	// Self-Checkout Station Card Reader Scene
+	
+	// Self-Checkout Station Maintenance Scene
+	
 }
