cbab90c118cf30022fcf108fc5a93e90c04b7355
Cloudy Young <cloudy.young@outlook.com>
2022-04-02 15:40:25 -0600

    Add more methods in Store to operate SCS

--- SCS - Software/src/store/Store.java
+++ SCS - Software/src/store/Store.java
@@ -1,5 +1,6 @@
 package store;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
@@ -7,9 +8,24 @@ import org.lsmr.selfcheckout.devices.SupervisionStation;
 
 
 public final class Store {
-    public static final SupervisionStation SUPERVISION_STATION = new SupervisionStation();
-    public static final List<SelfCheckoutStation> SELF_CHECKOUT_STATIONS = SUPERVISION_STATION.supervisedStations();
+    private static final SupervisionStation SUPERVISION_STATION = new SupervisionStation();
     
     private Store() {
+    }
+
+    public static List<SelfCheckoutStation> getSelfCheckoutStations() {
+        return Collections.unmodifiableList(SUPERVISION_STATION.supervisedStations());
+    }
+
+    public static SupervisionStation getSupervisionStation() {
+        return SUPERVISION_STATION;
+    }
+
+    public static void addSelfCheckoutStation(SelfCheckoutStation station) {
+        SUPERVISION_STATION.add(station);
+    }
+
+    public static void removeSelfCheckoutStation(SelfCheckoutStation station) {
+        SUPERVISION_STATION.remove(station);
     }
 }
