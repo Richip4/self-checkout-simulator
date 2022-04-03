ca8d517340d56bfda96bb853d2d9e422a133b53f
Cloudy Young <cloudy.young@outlook.com>
2022-04-02 21:42:26 -0600

    Update Store attributes and methods

--- SCS - Software/src/store/Store.java
+++ SCS - Software/src/store/Store.java
@@ -1,48 +1,30 @@
 package store;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
-import org.lsmr.selfcheckout.devices.SupervisionStation;
 
 import user.SelfCheckoutSoftware;
 import user.SupervisionSoftware;
 
 public final class Store {
-    private static final SupervisionStation SUPERVISION_STATION = new SupervisionStation();
-    private static final SupervisionSoftware SUPERVISION_SOFTWARE = new SupervisionSoftware(SUPERVISION_STATION);
-    private static final Map<SelfCheckoutStation, SelfCheckoutSoftware> SOFTWARE_MAP = new HashMap<SelfCheckoutStation, SelfCheckoutSoftware>();
-    
+    private static SupervisionSoftware SUPERVISION_SOFTWARE;
+
     private Store() {
     }
 
-    public static List<SelfCheckoutStation> getSelfCheckoutStations() {
-        return Collections.unmodifiableList(SUPERVISION_STATION.supervisedStations());
-    }
-
-    public static SupervisionStation getSupervisionStation() {
-        return SUPERVISION_STATION;
+    public static void setSupervisionSoftware(SupervisionSoftware supervisionSoftware) {
+        Store.SUPERVISION_SOFTWARE = supervisionSoftware;
     }
 
     public static SupervisionSoftware getSupervisionSoftware() {
         return SUPERVISION_SOFTWARE;
     }
 
-    public static void addSelfCheckoutStation(SelfCheckoutStation station) {
-        SUPERVISION_STATION.add(station);
-
-        // Create a new software object for the station.
-        SOFTWARE_MAP.put(station, new SelfCheckoutSoftware(station));
+    public static void addSelfCheckoutSoftware(SelfCheckoutSoftware software) {
+        SUPERVISION_SOFTWARE.add(software);
     }
 
-    public static void removeSelfCheckoutStation(SelfCheckoutStation station) {
-        SUPERVISION_STATION.remove(station);
-    }
-
-    public static SelfCheckoutSoftware getSelfCheckoutStationSoftware(SelfCheckoutStation station) {
-        return SOFTWARE_MAP.get(station);
+    public static List<SelfCheckoutSoftware> getSelfCheckoutSoftwareList() {
+        return Collections.unmodifiableList(SUPERVISION_SOFTWARE.getSoftwareList());
     }
 }
