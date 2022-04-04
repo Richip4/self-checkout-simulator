8b375a7cbb81324cf87330346d2bd76c18899629
Cloudy Young <cloudy.young@outlook.com>
2022-04-04 00:17:16 -0600

    Create Observer class and adapt for Software

---
+++ SCS - Software/src/software/Observer.java
@@ -0,0 +1,4 @@
+package software;
+
+public abstract interface Observer {
+}
--- SCS - Software/src/software/Software.java
+++ SCS - Software/src/software/Software.java
@@ -1,4 +1,38 @@
 package software;
 
-public abstract class Software {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * 
+ * @author Yunfan Yang
+ */
+public abstract class Software<T extends Observer> {
+    private List<T> observers = new ArrayList<T>();
+
+    public void addObserver(T observer) {
+        this.observers.add(observer);
+    }
+
+    public void removeObserver(T observer) {
+        this.observers.remove(observer);
+    }
+
+    public List<T> getObservers() {
+        return this.observers;
+    }
+
+    /**
+     * Usage: <pre>software.notifyObservers(observer -> observer.event(parameter));</pre>
+     * 
+     * @param function
+     * 
+     * @author Yunfan Yang
+     */
+    public void notifyObservers(Function<T, Void> function) {
+        for (T observer : observers) {
+            function.apply(observer);
+        }
+    }
 }
