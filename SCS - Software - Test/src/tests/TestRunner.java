05051c669850ebd6b12b43817a7ffd1b35fcad71
Sharjeel Junaid <sharjeelj@outlook.com>
2022-04-07 17:31:47 -0600

    Renamed MainTest (that runs all the tests) to TestRunner to avoid a name conflict

--- SCS - Software - Test/src/tests/TestRunner.java
+++ SCS - Software - Test/src/tests/TestRunner.java
@@ -7,28 +7,24 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         // All tests under tests.application
-
+        tests.application.AppControlTest.class, tests.application.MainTest.class,
         // All tests under tests.bank
         tests.bank.BankTest.class,
         // All tests under tests.checkout
-        tests.checkout.CheckoutTest.class, tests.checkout.ReceiptTest.class,
-        // All tests under tests.databases
-
+        tests.checkout.CheckoutTest.class, tests.checkout.ReceiptTest.class, tests.checkout.ScreenTest.class,
         // All tests under tests.gui
-
+        tests.gui.GUITest.class, tests.gui.ScenesTest.class,
         // All tests under tests.interrupt
         tests.interrupt.BanknoteHandlerTest.class, tests.interrupt.CardHandlerTest.class, tests.interrupt.CoinHandlerTest.class, tests.interrupt.ProcessItemHandlerTest.class,
         // All tests under tests.software
-
-        // All tests under tests.software.observers
-
+        tests.software.SelfCheckoutSoftwareTest.class, tests.software.SupervisionSoftwareTest.class,
         // All tests under tests.store
         tests.store.GiftCardTest.class, tests.store.InventoryTest.class, tests.store.MembershipTest.class, tests.store.StoreTest.class,
         // All tests under tests.store.credentials
         tests.store.credentials.CredentialsSystemTest.class,
         // All tests under tests.user
         tests.user.AttendantTest.class, tests.user.CustomerTest.class})
-public class MainTest
+public class TestRunner
 {
 
 }
