c08279dc3a6db02906ff9b79374fa1f81db247d4
Sharjeel Junaid <sharjeelj@outlook.com>
2022-04-07 00:03:34 -0600

    Update MainTest to accommodate the new / changed tests

--- SCS - Software - Test/src/tests/MainTest.java
+++ SCS - Software - Test/src/tests/MainTest.java
@@ -6,16 +6,24 @@ import org.junit.runners.Suite;
 // Main class that runs all the other tests
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+        // All tests under tests.application
+
         // All tests under tests.bank
         tests.bank.BankTest.class,
         // All tests under tests.checkout
-        tests.checkout.CardHandlerTest.class, tests.checkout.CheckoutTest.class, tests.checkout.ReceiptTest.class,
-        // All tests under tests.interupt
-        tests.interupt.BanknoteHandlerTest.class, tests.interupt.CoinHandlerTest.class, tests.interupt.ProcessItemHandlerTest.class,
+        tests.checkout.CheckoutTest.class, tests.checkout.ReceiptTest.class,
+        // All tests under tests.databases
+
+        // All tests under tests.gui
+
+        // All tests under tests.interrupt
+        tests.interrupt.BanknoteHandlerTest.class, tests.interrupt.CardHandlerTest.class, tests.interrupt.CoinHandlerTest.class, tests.interrupt.ProcessItemHandlerTest.class,
+        // All tests under tests.software
+
         // All tests under tests.store
-        tests.store.InventoryTest.class, tests.store.MembershipTest.class,
+        tests.store.InventoryTest.class, tests.store.MembershipTest.class, tests.store.StoreTest.class,
         // All tests under tests.user
-        tests.user.CustomerTest.class})
+        tests.user.AttendantTest.class, tests.user.CustomerTest.class})
 public class MainTest
 {
 
