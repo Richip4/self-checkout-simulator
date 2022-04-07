490cd2efbf7e6e283593a2df77ddbfea1c593f19
Sharjeel Junaid <sharjeelj@outlook.com>
2022-04-06 21:03:54 -0600

    Added reworked Membership tests

--- SCS - Software - Test/src/tests/store/MembershipTest.java
+++ SCS - Software - Test/src/tests/store/MembershipTest.java
@@ -12,18 +12,41 @@ public class MembershipTest
     // Static variables that will be used during testing
     final String membership1ID = "1";
     final String membership2ID = "2";
+    final String membership1Name = "Customer 1";
+    final String membership2Name = "Customer 2";
 
+    // Setup that is run before each test case
+    @Before
+    public void setup()
+    {
+        // Resets the membership information
+        Membership.clear();
+    }
 
     @Test
-    public void membershipSignupTest()
+    public void membershipSignupAndCheckTest()
     {
         assertFalse(Membership.isMember(membership1ID));
         assertFalse(Membership.isMember(membership2ID));
 
-        Membership.createMembership(membership1ID, "Joshua");
-        Membership.createMembership(membership2ID, "Gagan");
+        Membership.createMembership(membership1ID, membership1Name);
+        Membership.createMembership(membership2ID, membership2Name);
 
         assertTrue(Membership.isMember(membership1ID));
         assertTrue(Membership.isMember(membership2ID));
+    }
+
+    @Test
+    public void membershipClearTest()
+    {
+        assertFalse(Membership.isMember(membership1ID));
+        assertFalse(Membership.isMember(membership2ID));
+
+        Membership.createMembership(membership1ID, membership1Name);
+        Membership.createMembership(membership2ID, membership2Name);
+        Membership.clear();
+
+        assertFalse(Membership.isMember(membership1ID));
+        assertFalse(Membership.isMember(membership2ID));
     }
 }
