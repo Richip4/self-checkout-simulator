d3f9db90a89cf1e16ce75794ff9c4227181fb3d6
Tyler Chen <chentyler52@gmail.com>
2022-04-07 20:39:47 -0600

    fixed indentation

--- SCS - Software/src/software/SupervisionSoftware.java
+++ SCS - Software/src/software/SupervisionSoftware.java
@@ -20,62 +20,63 @@ import software.observers.SupervisionObserver;
  * handlers, the hardware devices have observers.
  * 
  * Use cases implemented here:
- * 	- Attendant logs in to their control console
- * 	- Attendant logs out from their control console
- *  - Attendant starts up a station
- * 	- Attendant sthuts down a station
+ * - Attendant logs in to their control console
+ * - Attendant logs out from their control console
+ * - Attendant starts up a station
+ * - Attendant sthuts down a station
  * 
  * 
  * @author Yunfan Yang
  * @author Tyler Chen
  */
 public class SupervisionSoftware extends Software<SupervisionObserver> {
-    private SupervisionStation svs;
-    private Attendant attendant;
-    private boolean logged_in;
-    private List<SelfCheckoutSoftware> softwareList = new ArrayList<SelfCheckoutSoftware>();
+	private SupervisionStation svs;
+	private Attendant attendant;
+	private boolean logged_in;
+	private List<SelfCheckoutSoftware> softwareList = new ArrayList<SelfCheckoutSoftware>();
 
-    
-    public SupervisionSoftware(SupervisionStation svs) {
-        this.svs = svs;
+	public SupervisionSoftware(SupervisionStation svs) {
+		this.svs = svs;
 
-        // TODO: Initialize handlers that supervision software needs.
-    }
+		// TODO: Initialize handlers that supervision software needs.
+	}
 
-	//For restarting a station, we don't want to restart the other stations too.
-	public SupervisionSoftware(SupervisionStation svs, List<SelfCheckoutSoftware> softwareList){
+	// For restarting a station, we don't want to restart the other stations too.
+	public SupervisionSoftware(SupervisionStation svs, List<SelfCheckoutSoftware> softwareList) {
 		this.svs = svs;
 		this.softwareList = softwareList;
 	}
 
-    public SupervisionStation getSupervisionStation() {
-        return this.svs;
-    }
-    
-    public Attendant getAttendant() {
-    	return this.attendant;
-    }
+	public SupervisionStation getSupervisionStation() {
+		return this.svs;
+	}
+
+	public Attendant getAttendant() {
+		return this.attendant;
+	}
 
 	/**
-	 * Sets the attendant. It's here because sometimes we don't have an immediate attendant
+	 * Sets the attendant. It's here because sometimes we don't have an immediate
+	 * attendant
 	 * at start up or attendant can change.
+	 * 
 	 * @param attendant
 	 */
-    public void setAttendant(Attendant attendant){
-        this.attendant = attendant;
-        this.logged_in = false;
-    }
+	public void setAttendant(Attendant attendant) {
+		this.attendant = attendant;
+		this.logged_in = false;
+	}
 
-    public void add(SelfCheckoutSoftware software) {
-        this.softwareList.add(software);
-        software.setSupervisionSoftware(this);
-    }
+	public void add(SelfCheckoutSoftware software) {
+		this.softwareList.add(software);
+		software.setSupervisionSoftware(this);
+	}
 
 	public void remove(SelfCheckoutSoftware software) {
 		this.softwareList.remove(software);
 		software.setSupervisionSoftware(null);
 	}
-	
+
 	public void clear() {
 		// For each software, remove from list
 		for (SelfCheckoutSoftware software : this.softwareList) {
@@ -83,62 +84,69 @@ public class SupervisionSoftware extends Software<SupervisionObserver> {
 		}
 	}
 
-    public List<SelfCheckoutSoftware> getSoftwareList() {
-        return this.softwareList;
-    }
+	public List<SelfCheckoutSoftware> getSoftwareList() {
+		return this.softwareList;
+	}
 
-    /**
-	 * Prompts the attendant for it's login creds, once that is complete, we get the username
-	 * and password and check the login. If the login was successful, we change the 
+	/**
+	 * Prompts the attendant for it's login creds, once that is complete, we get the
+	 * username
+	 * and password and check the login. If the login was successful, we change the
 	 * loggedIn flag.
+	 * 
 	 * @return T/F whether we've logged in successfully
 	 */
 	public void login() throws IncorrectCredentialException {
 		String username = attendant.getUsername();
 		String password = attendant.getPassword();
-		
-		if(CredentialsSystem.checkLogin(username, password)) {
+
+		if (CredentialsSystem.checkLogin(username, password)) {
 			this.logged_in = true;
 		} else {
 			throw new IncorrectCredentialException("Attendant credential is invalid");
 		}
 	}
-	
+
 	/**
 	 * Logout method to make sure that someone has logged out.
+	 * 
 	 * @return true if it successfully logs out
 	 */
 	public void logout() {
 		this.logged_in = false;
 	}
 
-    /**
-	 * Given the hardware, we need to start up the station for use. Assuming that the station
+	/**
+	 * Given the hardware, we need to start up the station for use. Assuming that
+	 * the station
 	 * has been shut off due to some reason.
+	 * 
 	 * @param scs - SelfCheckoutStation that it wants to boot up.
 	 * @return T/F - Whether the software has been booted.
-	 * TODO: Disable hardware? Boot up GUI? 
-	 * If it enables hardware, should it before or after? I don't think we do
-	 * because the hardware team should have it enabled?
+	 *         TODO: Disable hardware? Boot up GUI?
+	 *         If it enables hardware, should it before or after? I don't think we
+	 *         do
+	 *         because the hardware team should have it enabled?
 	 */
-	public void startUpStation(SelfCheckoutSoftware scss) throws AuthorizationRequiredException  {
+	public void startUpStation(SelfCheckoutSoftware scss) throws AuthorizationRequiredException {
 		if (this.logged_in) {
-			scss.startSystem();	
-		}else {
+			scss.startSystem();
+		} else {
 			throw new AuthorizationRequiredException("Attendant needs to log in");
 		}
 	}
 
-    /**
-	 * Shuts down the software simply by removing it from the HashMap of checkoutStations
+	/**
+	 * Shuts down the software simply by removing it from the HashMap of
+	 * checkoutStations
 	 * 
 	 * @return T/F - whether the checkoutStation has been removed. (If false
-	 * the station most likely is not in the HashMap not exist)
+	 *         the station most likely is not in the HashMap not exist)
 	 */
 	public void shutDownStation(SelfCheckoutSoftware scss) throws AuthorizationRequiredException {
-        if (this.logged_in) {
+		if (this.logged_in) {
 			scss.stopSystem();
-		}else {
+		} else {
 			throw new AuthorizationRequiredException("Attendant needs to log in");
 		}
 	}
@@ -151,13 +159,13 @@ public class SupervisionSoftware extends Software<SupervisionObserver> {
 	 * @return T/F whether the station has been blocked.
 	 */
 	public void blockStation(SelfCheckoutSoftware scss) throws AuthorizationRequiredException {
-		if (this.logged_in){
+		if (this.logged_in) {
 			scss.blockSystem();
-		}else{
+		} else {
 			throw new AuthorizationRequiredException("Attendant needs to log in");
 		}
 	}
-	
+
 	public void unblockStation(SelfCheckoutSoftware scss) throws AuthorizationRequiredException {
 		if (this.logged_in) {
 			scss.unblockSystem();
@@ -177,16 +185,17 @@ public class SupervisionSoftware extends Software<SupervisionObserver> {
 	/**
 	 * Start up the SupervisionSoftware, only by setting the store
 	 */
-	public void startUp(){
+	public void startUp() {
 		Store.setSupervisionSoftware(this);
 	}
 
 	/**
 	 * Should only be used when a full shut down is needed.
 	 * WARNING: Loses all SelfCheckoutSoftware
+	 * 
 	 * @throws AuthorizationRequiredException
 	 */
-	public void shutdown() throws AuthorizationRequiredException{
+	public void shutdown() throws AuthorizationRequiredException {
 		if (this.logged_in) {
 			Store.setSupervisionSoftware(null);
 		} else {
@@ -195,11 +204,13 @@ public class SupervisionSoftware extends Software<SupervisionObserver> {
 	}
 
 	/**
-	 * Restarts the Supervision Software while keeping the list of SelfCheckoutSoftware
+	 * Restarts the Supervision Software while keeping the list of
+	 * SelfCheckoutSoftware
+	 * 
 	 * @throws AuthorizationRequiredException
 	 */
-	public void restart() throws AuthorizationRequiredException{
-		if (this.logged_in){
+	public void restart() throws AuthorizationRequiredException {
+		if (this.logged_in) {
 			SupervisionSoftware scss = new SupervisionSoftware(svs, softwareList);
 			Store.setSupervisionSoftware(scss);
 		} else {
@@ -207,4 +218,3 @@ public class SupervisionSoftware extends Software<SupervisionObserver> {
 		}
 	}
 }
-
