725e8bd4a893fb5ddf48e3ec3e60ba757ca4bfad
khaledm99 <khaled.mograbee@ucalgary.ca>
2022-04-10 11:27:54 -0600

    Changed initializer methods in Main to public, added some tests to AppControlTest

--- SCS - Software - Test/src/tests/application/AppControlTest.java
+++ SCS - Software - Test/src/tests/application/AppControlTest.java
@@ -1,4 +1,184 @@
 package tests.application;
 
-public class AppControlTest
-{}
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import application.AppControl;
+import application.Main;
+import user.Attendant;
+import user.Customer;
+import user.User;
+
+public class AppControlTest {
+	
+	@Before
+	public void setup() {
+        Main.initializeCardAndIssuers();
+        Main.initializeProductDatabase();
+        Main.initializeStore();
+        Main.initializeMembership();
+        Main.initializeCredentialsSytem();
+	}
+	
+	@Test
+	public void testConstructor() {
+       
+		AppControl ac = new AppControl();
+		System.out.println(ac.getActiveUser());
+		assertTrue(true);
+	}
+
+	@Test
+	public void testAddNewCustomer() {
+		//Main m = new Main();
+		//m.main(null);
+		AppControl ac = new AppControl();
+		ac.addNewCustomer();
+		ac.getActiveUser();
+		assertTrue(ac.getActiveUser() instanceof Customer);
+	}
+
+	@Test
+	public void testAddNewAttendant() {
+		//Main m = new Main();
+		//m.main(null);
+		AppControl ac = new AppControl();
+		ac.addNewAttendant();
+		ac.getActiveUser();
+		assertTrue(ac.getActiveUser() instanceof Attendant);
+	}
+
+	@Test
+	public void testNextActiveUser() {
+		//Main m = new Main();
+		//m.main(null);
+		AppControl ac = new AppControl();
+		ac.addNewCustomer();
+		User customer1 = ac.getActiveUser();
+		ac.addNewCustomer();
+		User customer2 = ac.getActiveUser();
+		assertTrue(customer1 != customer2);
+		assertTrue(ac.getActiveUser() == customer2);
+		ac.nextActiveUser();
+		assertTrue(ac.getActiveUser() == customer1);
+	}
+
+	@Test
+	public void testPrevActiveUser() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetActiveUser() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetUserAt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetActiveUsers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCustomerUsesStation() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAttendantUsesStation() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCustomerLeavesStation() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAttendantLeavesStation() {
+		fail("Not yet implemented"); // TODO
+	}
+	@Test
+	public void testcustomerTapsCreditCard() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testcustomerTapsDebitCard() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testcustomerTapsMembershipCard() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testcustomerSwipesCreditCard() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testcustomerSwipesDebitCard() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testcustomerSwipesMembershipCard() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testcustomerInsertCreditCard() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testcustomerInsertDebitCard() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testcustomerInsertMembershipCard() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testremoveFromCart() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testattendantLogin() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testattendantPassword() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testgetCart() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testAttendantIsLoggedIn() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Test
+	public void testCustomerNextItem() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
