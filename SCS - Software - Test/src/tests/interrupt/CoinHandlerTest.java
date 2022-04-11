a9e4993aba5bf8c1ce865399f761f5235d24c278
Sharjeel Junaid <sharjeelj@outlook.com>
2022-04-10 21:06:07 -0600

    Added tests for CoinHandler

--- SCS - Software - Test/src/tests/interrupt/CoinHandlerTest.java
+++ SCS - Software - Test/src/tests/interrupt/CoinHandlerTest.java
@@ -2,340 +2,182 @@ package tests.interrupt;
 
 
 import interrupt.CoinHandler;
+import org.junit.Before;
 import org.junit.Test;
 import org.lsmr.selfcheckout.Coin;
-import org.lsmr.selfcheckout.devices.*;
+import org.lsmr.selfcheckout.devices.DisabledException;
+import org.lsmr.selfcheckout.devices.OverloadException;
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+import org.lsmr.selfcheckout.devices.SupervisionStation;
 import software.SelfCheckoutSoftware;
+import software.SupervisionSoftware;
+import store.Store;
 import user.Customer;
 
 import java.math.BigDecimal;
 import java.util.Currency;
-import java.util.List;
 
 import static org.junit.Assert.*;
 
-/**
- * The JUnit test class for the CoinHandler class in SCS - Software.
- *
- * @author Ricky Bhatti
- * @author Michelle Cheung
- */
 public class CoinHandlerTest
 {
-    Currency currency = Currency.getInstance("USD");
-    int[] banknoteDenominations = {1, 5, 10, 25, 100};
-    BigDecimal[] coinDenominations = {new BigDecimal("0.01"), new BigDecimal("0.05"), new BigDecimal("0.1"), new BigDecimal("0.25"), new BigDecimal("1.00")};
-    List <BigDecimal> coinDenominationsList = java.util.Arrays.asList(coinDenominations);
-    int scaleMaximumWeight = 10;
-    int scaleSensitivity = 1;
-    SelfCheckoutStation selfCheckoutStation = new SelfCheckoutStation(currency, banknoteDenominations, coinDenominations, scaleMaximumWeight, scaleSensitivity);
-    SelfCheckoutSoftware selfCheckoutSoftware = new SelfCheckoutSoftware(selfCheckoutStation);
-    CoinSlot coinSlot = new CoinSlot();
-    CoinTray coinTray = new CoinTray(10);
-    CoinValidator coinValidator = new CoinValidator(currency, coinDenominationsList);
-    CoinStorageUnit coinStorageUnit = new CoinStorageUnit(10);
-    CoinDispenser coinDispenser = new CoinDispenser(10);
-    Coin coin = new Coin(currency, new BigDecimal("0.01"));
+    // Static variables that will be used during testing
+    final Currency currency = Currency.getInstance("CAD");
+    final int[] banknoteDenominations = {5, 10, 20, 50};
+    final BigDecimal[] coinDenominations = {new BigDecimal("0.05"), new BigDecimal("0.10"), new BigDecimal("0.25"), new BigDecimal("1.00"), new BigDecimal("2.00")};
+    final int scaleMaximumWeight = 100;
+    final int scaleSensitivity = 10;
 
-    @Test
-    public void CoinHandlerTest()
+    SelfCheckoutStation selfCheckoutStation;
+    SelfCheckoutSoftware selfCheckoutSoftware;
+    SupervisionStation supervisionStation;
+    SupervisionSoftware supervisionSoftware;
+
+    CoinHandler coinHandler;
+    Customer customer;
+
+    Coin coin1;
+    Coin coin2;
+
+    @Before
+    public void setup()
     {
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        assertNotNull(coinHandler);
+        selfCheckoutStation = new SelfCheckoutStation(currency, banknoteDenominations, coinDenominations, scaleMaximumWeight, scaleSensitivity);
+        selfCheckoutSoftware = new SelfCheckoutSoftware(selfCheckoutStation);
+        supervisionStation = new SupervisionStation();
+        supervisionSoftware = new SupervisionSoftware(supervisionStation);
+        supervisionSoftware.add(selfCheckoutSoftware);
+        coinHandler = new CoinHandler(selfCheckoutSoftware);
+        customer = new Customer();
+        coinHandler.setCustomer(customer);
+        coin1 = new Coin(currency, coinDenominations[2]);
+        coin2 = new Coin(currency, coinDenominations[3]);
+        Store.setSupervisionSoftware(supervisionSoftware);
+        Store.addSelfCheckoutSoftware(selfCheckoutSoftware);
     }
 
     @Test
-    public void CoinHandlerTest2()
+    public void attachAndDetachTest()
     {
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        assertNotNull(coinHandler);
+        selfCheckoutStation.coinSlot.detach(coinHandler);
+        selfCheckoutStation.coinDispensers.forEach((k, v) -> v.detach(coinHandler));
+        selfCheckoutStation.coinStorage.detach(coinHandler);
+        selfCheckoutStation.coinTray.detach(coinHandler);
+        selfCheckoutStation.coinValidator.detach(coinHandler);
+
+        assertFalse(selfCheckoutStation.coinSlot.detach(coinHandler));
+        selfCheckoutStation.coinDispensers.forEach((k, v) -> assertFalse(v.detach(coinHandler)));
+        assertFalse(selfCheckoutStation.coinStorage.detach(coinHandler));
+        assertFalse(selfCheckoutStation.coinTray.detach(coinHandler));
+        assertFalse(selfCheckoutStation.coinValidator.detach(coinHandler));
+
+        coinHandler.attachAll();
+
+        assertTrue(selfCheckoutStation.coinSlot.detach(coinHandler));
+        selfCheckoutStation.coinDispensers.forEach((k, v) -> assertTrue(v.detach(coinHandler)));
+        assertTrue(selfCheckoutStation.coinStorage.detach(coinHandler));
+        assertTrue(selfCheckoutStation.coinTray.detach(coinHandler));
+        assertTrue(selfCheckoutStation.coinValidator.detach(coinHandler));
+
+        coinHandler.attachAll();
+        coinHandler.detatchAll();
+
+        assertFalse(selfCheckoutStation.coinSlot.detach(coinHandler));
+        selfCheckoutStation.coinDispensers.forEach((k, v) -> assertFalse(v.detach(coinHandler)));
+        assertFalse(selfCheckoutStation.coinStorage.detach(coinHandler));
+        assertFalse(selfCheckoutStation.coinTray.detach(coinHandler));
+        assertFalse(selfCheckoutStation.coinValidator.detach(coinHandler));
+    }
+
+    @Test
+    public void enableAndDisableHardwareTest()
+    {
+        assertFalse(selfCheckoutStation.coinSlot.isDisabled());
+        assertFalse(selfCheckoutStation.coinTray.isDisabled());
+        assertFalse(selfCheckoutStation.coinStorage.isDisabled());
+        assertFalse(selfCheckoutStation.coinValidator.isDisabled());
+
+        coinHandler.disableHardware();
+
+        assertTrue(selfCheckoutStation.coinSlot.isDisabled());
+        assertTrue(selfCheckoutStation.coinTray.isDisabled());
+        assertTrue(selfCheckoutStation.coinStorage.isDisabled());
+        assertTrue(selfCheckoutStation.coinValidator.isDisabled());
+
+        coinHandler.enableHardware();
+
+        assertFalse(selfCheckoutStation.coinSlot.isDisabled());
+        assertFalse(selfCheckoutStation.coinTray.isDisabled());
+        assertFalse(selfCheckoutStation.coinStorage.isDisabled());
+        assertFalse(selfCheckoutStation.coinValidator.isDisabled());
     }
 
     @Test
     public void getCustomerTest()
     {
-        Customer customer = new Customer();
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.setCustomer(customer);
         assertEquals(customer, coinHandler.getCustomer());
     }
 
     @Test
-    public void setCustomerTest()
+    public void insertAndGetCoinTest() throws OverloadException, DisabledException
     {
-        Customer customer = new Customer();
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.setCustomer(customer);
-        assertEquals(customer, coinHandler.getCustomer());
-    }
+        assertFalse(coinHandler.getCoinDetected());
 
-    @Test
-    public void coinInsertedTest()
-    {
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinInserted(coinSlot);
+        selfCheckoutStation.coinSlot.accept(coin1);
+
         assertTrue(coinHandler.getCoinDetected());
+        assertEquals(coin1.getValue(), coinHandler.getCoinValue());
     }
 
     @Test
-    public void getCoinDetectedTest()
+    public void invalidCoinTest() throws OverloadException, DisabledException
     {
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinInserted(coinSlot);
-        assertTrue(coinHandler.getCoinDetected());
+        assertFalse(coinHandler.getCoinDetected());
+
+        selfCheckoutStation.coinSlot.accept(new Coin(currency, new BigDecimal(banknoteDenominations[0])));
+
+        assertFalse(coinHandler.getCoinDetected());
+        assertEquals(BigDecimal.ZERO, coinHandler.getCoinValue());
     }
 
     @Test
-    public void coinAddedTest()
+    public void insertAndUnloadCoinTest() throws OverloadException, DisabledException
     {
-        // TODO: Write an actual test for this method, when implemented.
+        selfCheckoutStation.coinSlot.disable();
 
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinAdded(coinTray);
-        assertTrue(true);
-    }
+        selfCheckoutStation.coinStorage.accept(coin1);
+        selfCheckoutStation.coinStorage.accept(coin2);
 
-    //    @Test
-    //    public void getCoinDetectedIsValidTest()
-    //    {
-    //        Customer customer = new Customer();
-    //        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-    //        coinHandler.setCustomer(customer);
-    //        coinHandler.coinInserted(coinSlot);
-    //        coinHandler.validCoinDetected(coinValidator, new BigDecimal("0.01"));
-    //        assertTrue(coinHandler.getCoinDetectedIsValid());
-    //    }
+        assertTrue(selfCheckoutStation.coinSlot.isDisabled());
 
-    @Test
-    public void getCoinValueTest()
-    {
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinInserted(coinSlot);
-        coinHandler.validCoinDetected(coinValidator, new BigDecimal("0.01"));
-        assertEquals(new BigDecimal("0.01"), coinHandler.getCoinValue());
-    }
+        selfCheckoutStation.coinStorage.unload();
 
-    //    @Test
-    //    public void validCoinDetectedTest()
-    //    {
-    //        Customer customer = new Customer();
-    //        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-    //        coinHandler.setCustomer(customer);
-    //        coinHandler.coinInserted(coinSlot);
-    //        coinHandler.validCoinDetected(coinValidator, new BigDecimal("0.01"));
-    //        assertTrue(coinHandler.getCoinDetectedIsValid());
-    //    }
-
-    @Test
-    public void validCoinDetectedTest2()
-    {
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinInserted(coinSlot);
-        coinHandler.validCoinDetected(coinValidator, new BigDecimal("0.01"));
-        assertEquals(coinHandler.getCoinValue(), new BigDecimal("0.01"));
-    }
-
-    @Test
-    public void validCoinDetectedTest3()
-    {
-        Customer customer = new Customer();
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.setCustomer(customer);
-        coinHandler.validCoinDetected(coinValidator, new BigDecimal("0.01"));
-        assertEquals(coinHandler.getCoinValue(), new BigDecimal("0.01"));
-    }
-
-    @Test
-    public void invalidCoinDetectedTest()
-    {
-        Customer customer = new Customer();
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.setCustomer(customer);
-        coinHandler.coinInserted(coinSlot);
-        coinHandler.invalidCoinDetected(coinValidator);
-        assertTrue(true);
-    }
-
-    //    @Test
-    //    public void invalidCoinDetectedTest2()
-    //    { //null customer
-    //        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-    //        coinHandler.coinInserted(coinSlot);
-    //        coinHandler.invalidCoinDetected(coinValidator);
-    //        assertFalse(coinHandler.getCoinDetectedIsValid());
-    //    }
-    //
-    //    @Test
-    //    public void invalidCoinDetectedTest3()
-    //    { //no coin detected
-    //        Customer customer = new Customer();
-    //        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-    //        coinHandler.setCustomer(customer);
-    //        coinHandler.invalidCoinDetected(coinValidator);
-    //        assertFalse(coinHandler.getCoinDetectedIsValid());
-    //    }
-
-    @Test
-    public void coinsLoadedTest()
-    {
-        // TODO: Write an actual test for this method, when implemented.
-
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinsLoaded(coinStorageUnit);
-        assertTrue(true);
-    }
-
-    @Test
-    public void coinsUnloadedTest()
-    {
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinsUnloaded(coinStorageUnit);
         assertFalse(selfCheckoutStation.coinSlot.isDisabled());
     }
 
     @Test
-    public void coinsFullTest()
+    public void coinStorageFullTest()
     {
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinsFull(coinStorageUnit);
+        assertFalse(selfCheckoutStation.coinSlot.isDisabled());
+
+        coinHandler.coinsFull(selfCheckoutStation.coinStorage);
+
         assertTrue(selfCheckoutStation.coinSlot.isDisabled());
     }
 
     @Test
-    public void coinAddedTest2()
+    public void coinAddedTest() throws OverloadException, DisabledException
     {
-        Customer customer = new Customer();
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.setCustomer(customer);
-        coinHandler.coinInserted(coinSlot);
-        BigDecimal original = customer.getCashBalance();
-        coinHandler.validCoinDetected(coinValidator, new BigDecimal("0.01"));
-        coinHandler.coinAdded(coinStorageUnit);
-        BigDecimal newValue = customer.getCashBalance();
-        assertEquals(new BigDecimal("0.01"), newValue.subtract(original));
+        selfCheckoutStation.coinSlot.accept(coin1);
+
+        coinHandler.coinAdded(selfCheckoutStation.coinStorage);
+
+        assertEquals(coin1.getValue(), customer.getCashBalance());
     }
 
     @Test
-    public void coinAddedTest3()
-    { //null customer
-        Customer customer = new Customer();
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinInserted(coinSlot);
-        BigDecimal original = customer.getCashBalance();
-        coinHandler.validCoinDetected(coinValidator, new BigDecimal("0.01"));
-        coinHandler.coinAdded(coinStorageUnit);
-        BigDecimal newValue = customer.getCashBalance();
-        assertEquals(new BigDecimal("0"), newValue.subtract(original));
-    }
-
-    @Test
-    public void coinAddedTest4()
-    { //invalid coin
-        Customer customer = new Customer();
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.setCustomer(customer);
-        coinHandler.coinInserted(coinSlot);
-        coinHandler.invalidCoinDetected(coinValidator);
-        BigDecimal original = customer.getCashBalance();
-        coinHandler.coinAdded(coinStorageUnit);
-        BigDecimal newValue = customer.getCashBalance();
-        assertEquals(new BigDecimal("0"), newValue.subtract(original));
-    }
-
-    //    @Test
-    //    public void getCoinDispenserFull()
-    //    {
-    //        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-    //        assertEquals(false, coinHandler.getCoinDispenserFull());
-    //    }
-    //
-    //    @Test
-    //    public void coinsFullTest2()
-    //    {
-    //        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-    //        coinHandler.coinsFull(coinDispenser);
-    //        assertTrue(coinHandler.getCoinDispenserFull());
-    //    }
-
-    @Test
-    public void coinsEmptyTest()
+    public void coinsEmpty()
     {
-        // TODO: Write an actual test for this method, when implemented.
-
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinsEmpty(coinDispenser);
-        assertTrue(true);
-    }
-
-    @Test
-    public void coinAddedTest5()
-    { //normal
-        Customer customer = new Customer();
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.setCustomer(customer);
-        coinHandler.coinInserted(coinSlot);
-        BigDecimal original = customer.getCashBalance();
-        coinHandler.validCoinDetected(coinValidator, new BigDecimal("0.01"));
-        coinHandler.coinAdded(coinDispenser, coin);
-        BigDecimal newValue = customer.getCashBalance();
-        assertEquals(new BigDecimal("0.01"), newValue.subtract(original));
-    }
-
-    @Test
-    public void coinAddedTest6()
-    { //null customer
-        Customer customer = new Customer();
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinInserted(coinSlot);
-        BigDecimal original = customer.getCashBalance();
-        coinHandler.validCoinDetected(coinValidator, new BigDecimal("0.01"));
-        coinHandler.coinAdded(coinDispenser, coin);
-        BigDecimal newValue = customer.getCashBalance();
-        assertEquals(new BigDecimal("0"), newValue.subtract(original));
-    }
-
-    @Test
-    public void coinAddedTest7()
-    { //invalid coin
-        Customer customer = new Customer();
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.setCustomer(customer);
-        coinHandler.coinInserted(coinSlot);
-        coinHandler.invalidCoinDetected(coinValidator);
-        BigDecimal original = customer.getCashBalance();
-        coinHandler.coinAdded(coinDispenser, coin);
-        BigDecimal newValue = customer.getCashBalance();
-        assertEquals(new BigDecimal("0"), newValue.subtract(original));
-    }
-
-
-    @Test
-    public void coinRemovedTest()
-    {
-        // TODO: Write an actual test for this method, when implemented.
-
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinRemoved(coinDispenser, coin);
-        assertTrue(true);
-    }
-
-    @Test
-    public void coinsLoadedTest2()
-    {
-        // TODO: Write an actual test for this method, when implemented.
-
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinsLoaded(coinDispenser, coin);
-        assertTrue(true);
-    }
-
-    @Test
-    public void coinsUnloadedTest2()
-    {
-        // TODO: Write an actual test for this method, when implemented.
-
-        CoinHandler coinHandler = new CoinHandler(selfCheckoutSoftware);
-        coinHandler.coinsUnloaded(coinDispenser, coin);
-        assertTrue(true);
+        coinHandler.coinsEmpty(selfCheckoutStation.coinDispensers.get(0));
     }
 }
